61f96cdb2ee1b0511ba985e2f14c3d5bcee35a71
mona <hibaalaani@github.com>
2020-05-06 15:45:35 +0200

    change book api

--- JS/fetchBookStore.js
+++ JS/fetchBookStore.js
@@ -1,25 +1,25 @@
-var english = "https://api.myjson.com/bins/zyv02"
-var books = []
+var english = "https://api.jsonbin.io/b/5ea833484c87c3359a632938";
+// "https://api.myjson.com/bins/zyv02"
+var books = [];
 
 function bookFetch(url) {
-    fetch(url)
-        .then(response => {
-            console.log(response);
+  fetch(url)
+    .then((response) => {
+      console.log(response);
 
-            return response.json();
-        })
-        .then(data => {
-            books = data.books
+      return response.json();
+    })
+    .then((data) => {
+      books = data.books;
 
-            console.log(books)
-            bookList(books)
-            createEvent()
-            // createMoreInfo()
-            searchBottun(books)
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        })
-
+      console.log(books);
+      bookList(books);
+      createEvent();
+      // createMoreInfo()
+      searchBottun(books);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
-bookFetch(english)
+bookFetch(english);
